f8aca7b2e4ba86ab3dec4b31e5243133c6a37516
hltkvnc <hlt1985@gmail.com>
2022-07-14 00:33:38 +0300

    Başlangıç

---
+++ source/banner.php
@@ -0,0 +1,28 @@
+<section class="banner-one">
+            <div class="banner-one__carousel owl-carousel owl-theme">
+                <div class="item">
+                    <div class="banner-one__single" style="margin-bottom:200px">
+                    <video autoplay muted loop id="myVideo">
+                        <source src="images/resources/monoverse_video.mp4" type="video/mp4">
+                    </video>
+                        <div class="container">
+                            
+                            <div class="col-lg-6 col-md-8">
+                                <div class="banner-one__content">
+                                    <p class="banner-one__tag-line banner-one__is-animated"></p>
+                                    <!-- /.banner-one__tag-line -->
+                                    <h3 class="banner-one__title banner-one__is-animated"><br>
+                                        </h3><!-- /.banner-one__title -->
+                                    <p class="banner-one__text banner-one__is-animated">
+                                        <br>
+                                        </p><!-- /.banner-one__text -->
+                                   
+                                </div><!-- /.banner-one__content -->
+                            </div><!-- /.col-lg-6 -->
+                        </div><!-- /.container -->
+                    </div><!-- /.banner-one__single -->
+                </div><!-- /.item -->
+               
+            </div><!-- /.banner-one__carousel owl-carousel owl-theme -->
+
+        </section><!-- /.banner-one -->
---
+++ source/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon.ico">
+    <link rel="manifest" href="images/favicon/site.webmanifest">
+    <title>Monovers.com | Dijital Dünyadaki Rehberiniz</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i%7CWork+Sans:400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/hover-min.css">
+    <link rel="stylesheet" href="css/vegas.min.css">
+    <link rel="stylesheet" href="plugins/inchtech-icons/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <style>
+        #myVideo {
+        position: fixed;
+        right: 0;
+        margin-top:100px;
+        margin:auto;
+        width: 100%;
+        height: 100%;
+        
+        }
+    </style>
+</head>
+
+<body class="home-one">
+    <div class="preloader"><span></span></div><!-- /.preloader -->
+    <div class="page-wrapper">
+        <header class="site-header site-header__header-two ">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box">
+                        <a class="navbar-brand" href="index.php">
+                            <img src="images/resources/logo-dark.png" class="main-logo" alt="Monovers Logo" width="20%" />
+                        </a>
+                        <button class="menu-toggler" data-target=".main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box left-hav">
+                            <li class="current">
+                                <a href="index.html">Vizyon</a>
+                            </li>
+                            <li>
+                                <a href="about.html">Hizmetlerimiz</a>
+                            </li>
+                            <li>
+                                <a href="services.html">Referanslar</a>
+                            </li>
+                            <li>
+                                <a href="#">Kariyer</a>
+                            </li>
+                            <li><a href="contact.html">İletişim</a></li>
+                        </ul>
+                        
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.site-header -->
